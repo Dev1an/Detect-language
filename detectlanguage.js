da38132b6562336ab807b62bae432f235878cff9
Devian <info@devian.tk>
2015-06-24 13:14:30 +0200

    init

---
+++ detectlanguage.js
@@ -0,0 +1,12 @@
+Meteor.startup(function() {
+	var preferredLanguage = amplify.store('preferred-language')
+	if (preferredLanguage)
+		TAPi18n.setLanguage(preferredLanguage)
+	else
+		TAPi18n.setLanguage(navigator.language.split('-')[0])
+})
+
+TAPi18n.setLanguageAmplify = function(language) {
+	amplify.store('preferred-language', language
+	TAPi18n.setLanguage(language).always(TAPi18n._afterUILanguageChange)
+}
